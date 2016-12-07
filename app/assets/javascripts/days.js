b09ad0aa2b191be551f1331af6e55eec95eaa345
Elan Kvitko <elankvitko@Elans-iMac.local>
2016-12-07 11:07:20 -0500

    fix hours breaking

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -1,6 +1,6 @@
 $( '.weekday_hours_form' ).hide();
 
-$( '.change-hours-list-item' ).click( function( e ) {
+$( '.change-hours-list-item a' ).click( function( e ) {
   e.preventDefault();
   $( '.weekday_hours_form' ).toggle();
 });
