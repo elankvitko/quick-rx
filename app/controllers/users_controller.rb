74453e899afdf3c1a0fe505f60cafffbb39317ca
Elan Kvitko <elankvitko@Elans-iMac.local>
2016-12-09 14:20:38 -0500

    add security for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
   def show
-    @todos = Todo.where( user_id: current_user.id )
-    if @todos.empty? == false
-      @todos = @todos.sort_by { |todo| todo.id }
-    end
+    @user = User.find_by( id: params[ 'id' ] )
 
-    @todo = Todo.new
-    @day = Day.find_by( day: 'weekday' )
-    @item = Item.new
-    @all_items = Item.all.sort_by { |item| item.id }.reverse
+    if @user == nil && user_signed_in?
+      redirect_to user_path( current_user )
+    elsif !user_signed_in?
+      redirect_to new_user_session_path
+    elsif @user.id != current_user.id
+      redirect_to user_path( current_user )
+    else
+      if user_signed_in? && @user.id == current_user.id
+        @todos = Todo.where( user_id: current_user.id )
+        if @todos.empty? == false
+          @todos = @todos.sort_by { |todo| todo.id }
+        end
+
+        @todo = Todo.new
+        @day = Day.find_by( day: 'weekday' )
+        @item = Item.new
+        @all_items = Item.all.sort_by { |item| item.id }.reverse
+      else
+        redirect_to root_path
+      end
+    end
   end
 end
